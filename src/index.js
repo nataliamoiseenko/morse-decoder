805a70a658c57ec18f6575e2af2e917e05fcb934
nataliamoiseenko <natallia.moiseenko@gmail.com>
2022-08-11 23:06:02 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    return expr.split('**********').map(word => {
+        const decodedWord = [];
+        for (let i = 0; i < word.length / 10; i++) {
+            const morseLetter = [];
+            const letter = word.substr(10 * i, 10);
+
+            for (let j = 0; j < letter.length; j++) {
+                const symbol = letter.substr(2 * j, 2);
+                if (symbol === '10') {
+                    morseLetter.push('.')
+                } else if (symbol === '11') {
+                    morseLetter.push('-')
+                }
+            }
+
+            decodedWord.push(MORSE_TABLE[morseLetter.join('')]);
+        }
+
+        return decodedWord.join('');
+    }).join(' ');
 }
 
 module.exports = {
